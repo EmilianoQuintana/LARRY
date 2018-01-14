5d1f9f84eb08b62ec7b3a76f792d51c4b6946af5
Itamar Curiel <itamarcu@gmail.com>
2018-01-14 22:13:52 +0200

    Added a single space to fix a bug

--- src/Database/SQL.java
+++ src/Database/SQL.java
@@ -4,7 +4,7 @@ public class SQL
 {
     public static final String  CREATE_TABLE               = "CREATE TABLE",
                                 IF_NOT_EXISTS              = "IF NOT EXISTS",
-                                CREATE_TABLE_IF_NOT_EXISTS = SQL.CREATE_TABLE + SQL.IF_NOT_EXISTS,
+                                CREATE_TABLE_IF_NOT_EXISTS = SQL.CREATE_TABLE + " " + SQL.IF_NOT_EXISTS,
                                 NOT_NULL                   = "NOT NULL ",
                                 PRIMARY_KEY                = "PRIMARY KEY ",
                                 AUTO_INCREMENT             = "AUTO INCREMENT ",
